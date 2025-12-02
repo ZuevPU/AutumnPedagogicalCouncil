36f4edbacda51e9e627a58cc40aa053aa692cd67
Зуев Петр <zuevpu@Gmail.com>
2025-12-02 14:33:42 +0300

    1

---
+++ speakers.js
@@ -0,0 +1,137 @@
+/* speakers.js */
+/* Данные для автоматической генерации блока «Спикеры» для Семинара-практикума «Осенний ПедСовет» */
+
+const speakers = [
+    {
+        photo: "gallery/1.jpg",
+        name: "Зуев Петр",
+        position: "Руководитель управления цифрового обучения",
+        tag: "Культурный код/Наука",
+        topic: "«ПЕРЕЗАГРУЗКА СРЕДЫ: Как Культурный код и Наука становятся вектором развития педагогического сообщества»",
+        bullets: [],
+        quote: "",
+        ppt: "https://disk.yandex.ru/i/fUyvnr6Ck3X2MQ",
+        notes: ""
+    },
+    {
+        photo: "https://i.pinimg.com/originals/3f/59/bd/3f59bddd185f8313a8babdf7bb107e8a.jpg",
+        name: "Дмитриев Вадим",
+        position: "",
+        tag: "Коммуникация",
+        topic: "Построение связей и выстраивание коммуникации",
+        bullets: [],
+        quote: "",
+        ppt: "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FIOsjKjg%2Bc7Ec29sfPwJynAKdbp7zu8zLVhNWQ06bXRpqhah9gwc78YJtopSSQB3Lq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FВадим%20Дмитриев.pptx&name=Вадим%20Дмитриев.pptx&nosw=1",
+        notes: ""
+    },
+    {
+        photo: "https://i.pinimg.com/originals/3f/59/bd/3f59bddd185f8313a8babdf7bb107e8a.jpg",
+        name: "Зенина Анастасия",
+        position: "",
+        tag: "Актерское мастерство/Педагогика",
+        topic: "Приемы актерского мастерства в педагогике",
+        bullets: [],
+        quote: "",
+        ppt: "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FIOsjKjg%2Bc7Ec29sfPwJynAKdbp7zu8zLVhNWQ06bXRpqhah9gwc78YJtopSSQB3Lq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FЗенина%20Анастасия.pptx&name=Зенина%20Анастасия.pptx&nosw=1",
+        notes: ""
+    },
+    {
+        photo: "https://i.pinimg.com/originals/3f/59/bd/3f59bddd185f8313a8babdf7bb107e8a.jpg",
+        name: "Зенин Илья",
+        position: "",
+        tag: "Проекты/Партнерство",
+        topic: "Детско-взрослые проекты как инструмент выстраивания партнерства школы и семьи",
+        bullets: [],
+        quote: "",
+        ppt: "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FIOsjKjg%2Bc7Ec29sfPwJynAKdbp7zu8zLVhNWQ06bXRpqhah9gwc78YJtopSSQB3Lq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FЗенин%20Илья.pptx&name=Зенин%20Илья.pptx&nosw=1",
+        notes: ""
+    },
+    {
+        photo: "https://i.pinimg.com/originals/3f/59/bd/3f59bddd185f8313a8babdf7bb107e8a.jpg",
+        name: "Кузьменко Анна",
+        position: "",
+        tag: "Проектирование/Business Model Canvas",
+        topic: "«Образовательный продукт: используем Business Model Canvas для проектирования урока»",
+        bullets: [],
+        quote: "",
+        ppt: "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FIOsjKjg%2Bc7Ec29sfPwJynAKdbp7zu8zLVhNWQ06bXRpqhah9gwc78YJtopSSQB3Lq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FАнна%20Кузьменко.pptx&name=Анна%20Кузьменко.pptx&nosw=1",
+        notes: ""
+    },
+    {
+        photo: "https://i.pinimg.com/originals/3f/59/bd/3f59bddd185f8313a8babdf7bb107e8a.jpg",
+        name: "Завалина Анна",
+        position: "",
+        tag: "Экскурсия/Миссия",
+        topic: "Педагогическая экскурсия - пленэр «В поисках миссии педагога»",
+        bullets: [],
+        quote: "",
+        ppt: "",
+        notes: ""
+    },
+    {
+        photo: "https://i.pinimg.com/originals/3f/59/bd/3f59bddd185f8313a8babdf7bb107e8a.jpg",
+        name: "Демехина Алла",
+        position: "",
+        tag: "Ресурс/Благополучие",
+        topic: "«Секрет счастливых педагогов: пять способов повысить своё субъективное благополучие в профессии»",
+        bullets: [],
+        quote: "",
+        ppt: "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FIOsjKjg%2Bc7Ec29sfPwJynAKdbp7zu8zLVhNWQ06bXRpqhah9gwc78YJtopSSQB3Lq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FАлла%20Демехина.pptx&name=Алла%20Демехина.pptx",
+        notes: ""
+    },
+    {
+        photo: "https://i.pinimg.com/originals/3f/59/bd/3f59bddd185f8313a8babdf7bb107e8a.jpg",
+        name: "Дремов Николай",
+        position: "",
+        tag: "Аргументация",
+        topic: "«Педагогическая аргументация»",
+        bullets: [],
+        quote: "",
+        ppt: "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FIOsjKjg%2Bc7Ec29sfPwJynAKdbp7zu8zLVhNWQ06bXRpqhah9gwc78YJtopSSQB3Lq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FДремов_Н_С_Педагогическая_аргументация_pptx.pdf&name=Дремов_Н_С_Педагогическая_аргументация_pptx.pdf&nosw=1",
+        notes: ""
+    },
+    {
+        photo: "gallery/2.jpg",
+        name: "Ляшко Олег",
+        position: "",
+        tag: "Машук/Наставничество",
+        topic: "Эффект Машука: Учим тех, кто учит",
+        bullets: [],
+        quote: "",
+        ppt: "",
+        notes: ""
+    },
+    {
+        photo: "gallery/4.jpg",
+        name: "Иванов Николай",
+        position: "Генеральный продюсер сериала Нейроны, директор анимационной студии «ChildGood animation»",
+        tag: "Метавселенная/Цифра",
+        topic: "«Метавселенная образования: виртуальное креативное пространство для учебного процесса»",
+        bullets: [],
+        quote: "",
+        ppt: "",
+        notes: ""
+    },
+    {
+        photo: "gallery/5.jpg",
+        name: "Казанцева Лилия",
+        position: "сопредседатель Всероссийского сообщества наставников-просветителей, учитель географии гимназии № 18 г. Краснодара Краснодарского края",
+        tag: "Сообщество/Наставники",
+        topic: "«Профессиональные сообщества как новый вид педагогической культуры»",
+        bullets: [],
+        quote: "",
+        ppt: "",
+        notes: ""
+    },
+    {
+        photo: "gallery/3.jpg",
+        name: "Ольга Самсонова и Альберт Феррер",
+        position: "методист-эксперт АО «Издательство «Просвещение» (О.Самсонова)",
+        tag: "Методики/Игры",
+        topic: "«Обучение вне шаблона: как «ледоколы», игры и другие приёмы меняют школьную реальность»",
+        bullets: [],
+        quote: "",
+        ppt: "",
+        notes: ""
+    }
+];
